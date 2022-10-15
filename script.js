54fd4ed68baf8972c387cd83ddf2c18a8ca5aff3
fryn4538 <fredrik.yngve.4538@student.uu.se>
2022-10-15 13:57:00 +0200

    done

--- script.js
+++ script.js
@@ -6,59 +6,64 @@ var dC = document.getElementById("dC");
 var hC = document.getElementById("hC");
 var mC = document.getElementById("mC");
 var sC = document.getElementById("sC");
+var league = document.getElementById("league");
+var cup = document.getElementById("cup");
 
-function refreshData()
-{
-    x = 1;  // 5 Seconds
+function refreshData() {
+  x = 1; // 5 Seconds
 
-    latestLeague();
-    latestCup();
+  latestLeague();
+  latestCup();
 
-    setTimeout(refreshData, x*1000);
+  setTimeout(refreshData, x * 1000);
 }
 
-function latestLeague()
-{
-    var str_late = "Sun, Aug 8 16:30";
-    var strr = str_late.substr(4,str_late.length) + " 2022";
-    var latest = new Date(strr);
+function latestLeague() {
+  var date = "";
+  var s = "";
+  fetch(
+    "https://raw.githubusercontent.com/fryn4538/sirius-counter/main/latest.json"
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      date = json["date"];
+      s = json["result"];
+      var latest = new Date(date);
 
-    var t = convertTime(latest);
-    
-    dL.innerHTML = t[0];
-    hL.innerHTML = t[1];
-    mL.innerHTML = t[2];
-    sL.innerHTML = t[3];
+      var t = convertTime(latest);
+
+      dL.innerHTML = t[0];
+      hL.innerHTML = t[1];
+      mL.innerHTML = t[2];
+      sL.innerHTML = t[3];
+
+      league.innerHTML = s;
+    });
 }
 
-function latestCup()
-{
-    var str_late = "Sun, Sep 1 18:30";
-    var strr = str_late.substr(4,str_late.length) + " 2022";
-    var latest = new Date(strr);
+function latestCup() {
+  var str_late = "Sun, Sep 1 18:30";
+  var strr = str_late.substr(4, str_late.length) + " 2022";
+  var latest = new Date(strr);
 
-    var t = convertTime(latest);
-    
-    dC.innerHTML = t[0];
-    hC.innerHTML = t[1];
-    mC.innerHTML = t[2];
-    sC.innerHTML = t[3];
+  var t = convertTime(latest);
+
+  dC.innerHTML = t[0];
+  hC.innerHTML = t[1];
+  mC.innerHTML = t[2];
+  sC.innerHTML = t[3];
 }
 
-function convertTime(date)
-{
-    var current = new Date();
+function convertTime(date) {
+  var current = new Date();
 
-    const diffTime = Math.abs(current - date);
-    var dT = Math.floor(diffTime / 8.64e7);
-    var hT = Math.floor(diffTime % 8.64e7 / 3.6e6);
-    var mT = Math.floor(diffTime % 3.6e6 / 6e4);
-    var sT = Math.floor(diffTime % 6e4 / 1e3);
+  const diffTime = Math.abs(current - date);
+  var dT = Math.floor(diffTime / 8.64e7);
+  var hT = Math.floor((diffTime % 8.64e7) / 3.6e6);
+  var mT = Math.floor((diffTime % 3.6e6) / 6e4);
+  var sT = Math.floor((diffTime % 6e4) / 1e3);
 
-    return [dT, hT, mT, sT];
+  return [dT, hT, mT, sT];
 }
-
 
 refreshData();
-
-
